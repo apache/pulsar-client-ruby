e61e00227c66a5654be1a305a0b7b1b799b2c52a
Jacob Fugal <jacob@instructure.com>
2019-10-02 09:57:35 -0600

    handle Pulsar::ConsumerConfiguration.consumer_type
    still allow passing in the enum value integer directly, but also for
    convenience translate a set of symbols too and from the enum values

---
+++ lib/pulsar/consumer_configuration.rb
@@ -0,0 +1,55 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+require 'pulsar/client/version'
+require 'pulsar/bindings'
+require 'pulsar/client_configuration'
+require 'pulsar/consumer'
+require 'pulsar/producer'
+
+module Pulsar
+  class ConsumerConfiguration
+    # aligns with the pulsar::ConsumerType enum in the C++ library
+    CONSUMER_TYPES = [
+      :exclusive,
+      :shared,
+      :failover,
+      :key_shared
+    ]
+
+    module RubySideTweaks
+      def consumer_type
+        enum_value = super
+        CONSUMER_TYPES[enum_value]
+      end
+
+      def consumer_type=(type)
+        unless type.is_a?(Integer)
+          type = CONSUMER_TYPES.index(type)
+          unless type
+            raise ArgumentError, "unrecognized consumer_type"
+          end
+        end
+        super(type)
+      end
+    end
+
+    prepend RubySideTweaks
+  end
+end
