b76aacb3344bac838c29f0ce6ac18f40f41230b7
Randy Stauner <randy@r4s6.net>
2021-02-18 15:58:40 -0700

    Do live pulsar produce/consume in tests when configured

--- spec/pulsar/client_spec.rb
+++ spec/pulsar/client_spec.rb
@@ -21,4 +21,31 @@ RSpec.describe Pulsar::Client do
   it "has a version number" do
     expect(Pulsar::Client::VERSION).not_to be nil
   end
+
+  context 'with pulsar' do
+    let(:broker_uri) { ENV['PULSAR_BROKER_URI'] }
+    let(:namespace) { ENV['PULSAR_CLIENT_RUBY_TEST_NAMESPACE'].to_s.sub(%r{^[-a-z]:/+}, '') }
+    let(:configured?) { broker_uri && !namespace.empty? }
+    let(:client) { Pulsar::Client.from_environment(broker_uri: broker_uri) }
+    let(:topic) { "non-persistent://#{namespace}/test#{sprintf "%06d", rand(1_000_000)}" }
+    let(:producer) { client.create_producer(topic) }
+    let(:subscription_name) { "#{topic}-consumer" }
+    let(:timeout_ms) { 10_000 }
+
+    before(:each) do
+      skip('Live Pulsar tests not configured: Set PULSAR_CLIENT_RUBY_TEST_NAMESPACE to enable') unless configured?
+    end
+
+    after(:each) do
+      # Close any producers/consumers to avoid test pollution.
+      client.close
+    end
+
+    it "can consume a single topic" do
+      consumer = client.subscribe(topic, subscription_name)
+      t = Thread.new { consumer.receive(timeout_ms).data }
+      client.create_producer(topic).send("single")
+      expect(t.join.value).to eq("single")
+    end
+  end
 end
